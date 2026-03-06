a7be0306ae369e07fa294e514357dffa4eca631d
Naeem-builds <naeem240720@gmail.com>
2026-03-07 01:10:40 +0500

    :sparkles: Describe what you changed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from langchain-chatbot!")
+from langchain_ollama import ChatOllama
 
+llm = ChatOllama(
+    model="qwen2.5:3b",
+    temperature=0.7
+    )
 
-if __name__ == "__main__":
-    main()
+response = llm.invoke("What is the SAN?")
+print(response.content)
